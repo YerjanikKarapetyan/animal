c1c54a15ff2c4b65f9b9ba28042572139aac3e12
Yerjanik <yerjanikkarapetyan@gmail.com>
2021-10-24 00:47:00 +0400

    initial commit

---
+++ src/containers/navbar/services/Calculator/Calculator.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import './Calculator.css';
+
+const Calculator = () =>{
+
+    return(
+        <div className={'div-div'}>
+            <p>
+
+            ssssssssssssssssssssssssssss
+            </p>
+        </div>
+    )
+}
+
+export default Calculator;
