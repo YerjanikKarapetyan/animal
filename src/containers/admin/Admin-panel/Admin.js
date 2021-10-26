01d600d001ec443074ae0aa8d9e2d5440ac169de
Yerjanik <yerjanikkarapetyan@gmail.com>
2021-10-26 23:53:31 +0400

    initial commit

---
+++ src/containers/admin/Admin-panel/Admin.js
@@ -0,0 +1,13 @@
+import React , {}from "react";
+import './Admin.css';
+import Login from "../Login/Login";
+
+const Admin = () =>{
+    
+
+    return(
+        <div>
+           
+        </div>
+    )
+}
